1c60d3fc81a9a369acec42c53551d6a2d5f657ea
Reginaldo Luis <jr.design_2010@hotmail.com>
2014-06-02 15:57:32 -0300

    configurando cakephp

--- framework/app/Config/routes.php
+++ framework/app/Config/routes.php
@@ -24,12 +24,13 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	//Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
-
+	//Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	Router::connect('/', array('controller' => 'home', 'action' => 'index'));
+	
 /**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.

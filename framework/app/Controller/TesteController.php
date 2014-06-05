2d2f3eb9883989c5f80b3d049673cc53507db66c
Reginaldo Luis <jr.design_2010@hotmail.com>
2014-06-05 13:46:27 -0300

    iniciando com session em cakephp

--- framework/app/Controller/TesteController.php
+++ framework/app/Controller/TesteController.php
@@ -11,15 +11,18 @@ class TesteController extends AppController{
 		$this->layout = 'teste';
 		echo $this->request->query['id_user'];
 		echo $this->request->query['nome_user'];
+
+		//dados da sessao
+		echo 'Sessaão pessoa: '. $this->Session->read('Pessoa.nome');
 		
 		echo 'listar';
 	}
 
 	function cadastrar(){
 		$this->request->data['cadastro']['cadastrar'] == null;
-		
+
 		if($this->request->data['cadastro']['cadastrar'] == 'cadastrar'){
-			echo $this->request->data['cadastro']['nome']."\n";
+			$this->Session->write('Pessoa.nome', $this->request->data['cadastro']['nome']);
 			echo $this->request->data['cadastro']['email']."\n";
 			echo $this->request->data['cadastro']['senha']."\n";
 		}

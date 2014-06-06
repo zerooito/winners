cdc7377eac7146a99feb220a1c66b245982aa645
Reginaldo Luis <jr.design_2010@hotmail.com>
2014-06-05 23:30:14 -0300

    session com verificao cakephp ok

--- framework/app/Controller/TesteController.php
+++ framework/app/Controller/TesteController.php
@@ -24,12 +24,20 @@ class TesteController extends AppController{
 	}
 
 	function cadastrar(){
-		$this->request->data['cadastro']['cadastrar'] == null;
+		//$this->request->data['cadastro']['cadastrar'] = null;
 
 		if($this->request->data['cadastro']['cadastrar'] == 'cadastrar'){
 			$this->Session->write('Pessoa.nome', $this->request->data['cadastro']['nome']);
-			echo $this->request->data['cadastro']['email']."\n";
-			echo $this->request->data['cadastro']['senha']."\n";
+			echo $this->Session->read('Pessoa.nome');
+			$this->Session->write('Pessoa.email',$this->request->data['cadastro']['email']);
+			echo $this->Session->read('Pessoa.email');
+			$this->Session->write('Pessoa.senha',$this->request->data['cadastro']['senha']);
+			echo $this->Session->read('Pessoa.senha');
+			if($this->Session->read('Pessoa.nome') == 'teste'){
+				echo 'teste ok';
+			}else{
+				echo 'ainda nao';
+			}
 		}
 
 		echo 'cadastrar';

2bc4f4231776e0b9bcf942509f6debfeecf69883
Reginaldo Luis <jr.design_2010@hotmail.com>
2014-06-13 12:50:12 -0300

    testes de session com array dinamico

--- framework/app/Controller/HomeController.php
+++ framework/app/Controller/HomeController.php
@@ -1,8 +1,18 @@
-<?php
+﻿<?php
 class HomeController extends AppController{
 	function index(){
 		$this->layout = 'winners';
+$produtos = array(array('nome' => 'produto1', 'id_produto' => '442'));
 
+print_r($produtos);
+
+array_push($produtos, array('nome' => 'produto7', 'id_produto' => '542'));
+
+print_r($produtos);
+
+foreach($produtos as $valor){
+	echo 'Nome produto: '. $valor['nome'].'<br>';
+}
 	}
 
 	function requisicoes(){

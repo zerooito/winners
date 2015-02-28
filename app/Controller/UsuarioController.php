30c72c66f90a6ee05de41b772363bdcba2fd7494
Reginaldo Junior <reginaldo.junior@betalabs.com.br>
2015-02-28 14:42:55 -0300

    ajustes controller

--- app/Controller/UsuarioController.php
+++ app/Controller/UsuarioController.php
@@ -125,4 +125,17 @@ class UsuarioController extends AppController{
 		$this->redirect('/');
 	}
 
+	public function enviar_email_sucesso($email, $nome) {	
+		$name = $dados['name'];
+		$email_address = $dados['email'];
+	
+		// Create the email and send the message
+		$to = 'winnersdevelopers@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+		$email_subject = "Contato Winners Desenvolvimento";
+		$email_body = "Muito Obrigado por nos contactar";
+		$headers = "From: noreply@winnersdevelopers.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+		$headers .= "Reply-To: $email_address";	
+		mail($to,$email_subject,$email_body,$headers);
+	}
+
 }

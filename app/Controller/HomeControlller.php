fcf8456af2c9a0b417ecb290b2e249a8766c7330
chucky6 <cristianojorgy@hotmail.com>
2015-02-28 12:49:46 -0300

    Email Teste

--- app/Controller/HomeControlller.php
+++ app/Controller/HomeControlller.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 class HomeController extends AppController{
 
 	public function beforeFilter(){
@@ -45,27 +45,12 @@ class HomeController extends AppController{
 	function enviar_email() {
 		$dados = $this->request->data('dados');
 
+		App::uses('CakeEmail', 'Network/Email');
 
-		// Check for empty fields
-		if(empty($dados['name'])  		||
-   			empty($dados['email']) 		||
-   			empty($dados['message'])	||
-   			!filter_var($dados['email'],FILTER_VALIDATE_EMAIL)){
-				echo "No arguments Provided!";
-				return false;
-			   }	
-	
-		$name = $dados['name'];
-		$email_address = $dados['email'];
-		$message = $dados['message'];
-	
-		// Create the email and send the message
-		$to = 'winnersdevelopers@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-		$email_subject = "Contato Winners Desenvolvimento";
-		$email_body = "Muito Obrigado por nos contactar";
-		$headers = "From: noreply@winnersdevelopers.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-		$headers .= "Reply-To: $email_address";	
-		mail($to,$email_subject,$email_body,$headers);
-		return true;			
+		$Email = new CakeEmail();
+		$Email->from(array('winnersdevelopers@gmail.com' => 'Winners'));
+		$Email->to($dados['email']);
+		$Email->subject('Contato Winners Desenvolvimento');
+		$Email->send('Muito obrigado');
 	}
 }

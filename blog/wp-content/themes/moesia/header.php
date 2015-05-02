c375e28045a7c50f7db2cfb12f51076ab61bc0ce
Reginaldo Junior <76regi@gmail.com>
2015-05-02 01:20:30 -0300

    analitycs ok

--- blog/wp-content/themes/moesia/header.php
+++ blog/wp-content/themes/moesia/header.php
@@ -35,6 +35,19 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60234652-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'moesia' ); ?></a>
 

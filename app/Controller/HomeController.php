f02881c57beafa54711e8c6c8e1d68554221eb3a
Reginaldo Junior <76regi@gmail.com>
2015-05-02 02:45:39 -0300

    contato winners ok

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -17,15 +17,14 @@ class HomeController extends AppController{
 	function enviar_email() {
 		$dados = $this->request->data('dados');
 
-		debug($dados);exit();
 		// Check for empty fields
 		if(empty($dados['name'])  		||
    			empty($dados['email']) 		||
    			empty($dados['message'])	||
    			!filter_var($dados['email'],FILTER_VALIDATE_EMAIL)){
-				echo "No arguments Provided!";
-				return false;
-			   }	
+				$this->Session->setFlash('Você deve preencher todos os dados!');
+	            return $this->redirect('/');
+		}	
 	
 		$name = $dados['name'];
 		$email_address = $dados['email'];
@@ -34,11 +33,20 @@ class HomeController extends AppController{
 		// Create the email and send the message
 		$to = 'winnersdevelopers@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 		$email_subject = "Contato Winners Desenvolvimento";
-		$email_body = "Muito Obrigado por nos contactar";
+		$email_body = "Mensagem site: ".$message;
 		$headers = "From: noreply@winnersdevelopers.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 		$headers .= "Reply-To: $email_address";	
 		mail($to,$email_subject,$email_body,$headers);
-		return true;			
+		//resposta automatica ao cliente
+		$to = $email_address;
+		$email_subject = 'winnersdevelopers@gmail.com';
+		$email_body = "A sua mensagem foi recebida, em breve responderemos";
+		$headers = "From: noreply@winnersdevelopers.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+		$headers .= "Reply-To: $email_address";	
+		mail($to,$email_subject,$email_body,$headers);
+
+		$this->Session->setFlash('Seu e-mail foi enviado com sucesso em breve responderemos!');
+        return $this->redirect('/');
 	}
 
 	// public function loja() {

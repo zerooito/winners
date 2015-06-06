d6dfa251768975f59351ab42486e79fff70fd996
Reginaldo Junior <76regi@gmail.com>
2015-06-06 11:11:28 -0300

    versao final

--- app/Controller/NfeController.php
+++ app/Controller/NfeController.php
@@ -21,10 +21,7 @@ class NfeController extends AppController {
             return $this->redirect('/nfe/gerar_danfe');			
    		}
 
-   		// pr($_FILES);
-		// move_uploaded_file($_FILES["nota"]["tmp_name"], 'nfephp/uploads/' . $_FILES["nota"]["name"]);
 		$arq = $_FILES["nota"]["tmp_name"];
-		// echo $arq;
 
 		if ( is_file($arq) ){
 			$docxml = file_get_contents($arq);

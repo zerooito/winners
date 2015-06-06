8f1cc65d19f4ea45216ebd69578ec86f973fc717
Reginaldo Junior <76regi@gmail.com>
2015-06-06 10:55:03 -0300

    foi ?

--- app/Controller/NfeController.php
+++ app/Controller/NfeController.php
@@ -16,12 +16,10 @@ class NfeController extends AppController {
    	public function danfe() {
    		require_once('nfephp/libs/NFe/DanfeNFePHP.class.php');
 
-   		pr($_FILES);
-   		exit();
-   // 		if ($_FILES['nota']['type'] != "text/xml") {
-			// $this->Session->setFlash('O arquivo deve ser do tipo xml! ', 'default');
-   //          return $this->redirect('/nfe/gerar_danfe');			
-   // 		}
+   		if ($_FILES['nota']['type'] != "text/xml") {
+			$this->Session->setFlash('O arquivo deve ser do tipo xml! ', 'default');
+            return $this->redirect('/nfe/gerar_danfe');			
+   		}
 
 		move_uploaded_file($_FILES["nota"]["tmp_name"], $_FILES["nota"]["name"]);
 		$arq = $_FILES["nota"]["name"];

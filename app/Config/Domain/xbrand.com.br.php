dbb99b2f8e49229aa9738ad65a8e1f942d70d143
Reginaldo Junior <76regi@gmail.com>
2015-07-04 15:44:41 -0300

    adicionando o dominio da xbrand

---
+++ app/Config/Domain/xbrand.com.br.php
@@ -0,0 +1,29 @@
+<?php
+
+$dominio = array(
+	'id_usuario' => '9',
+	'controller' => 'XStore',
+	'funcao'	 => 'index'
+);
+
+$_SESSION['information'] = $dominio;
+
+Router::connect('/addCart',   array('controller' => $dominio['controller'], 'action' => 'addCart'));
+
+Router::connect('/clearCart',   array('controller' => $dominio['controller'], 'action' => 'clearCart'));
+
+Router::connect('/cart',   array('controller' => $dominio['controller'], 'action' => 'cart'));
+
+Router::connect('/checkout',   array('controller' => $dominio['controller'], 'action' => 'checkout'));
+
+Router::connect('/payment',   array('controller' => $dominio['controller'], 'action' => 'payment'));
+
+Router::connect('/category/:id/:nome',   array('controller' => $dominio['controller'], 'action' => 'category'));
+
+Router::connect('/removeProductCart/:id',   array('controller' => $dominio['controller'], 'action' => 'removeProductCart'));
+
+Router::connect('/produto/:id',    array('controller' => $dominio['controller'], 'action' => 'produto'));
+
+require CAKE . 'Config' . DS . 'routes.php';
+
+CakePlugin::routes();

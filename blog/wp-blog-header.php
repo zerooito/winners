1c3824530501470085c6af85f281628d6a8fcc18
Reginaldo Junior <76regi@gmail.com>
2015-07-31 20:57:06 -0300

    put e delete client ok

--- blog/wp-blog-header.php
+++ blog/wp-blog-header.php
@@ -6,11 +6,10 @@
  */
 
 if ( !isset($wp_did_header) ) {
-
 	$wp_did_header = true;
 
 	require_once( dirname(__FILE__) . '/wp-load.php' );
-
+	
 	wp();
 	require_once( ABSPATH . WPINC . '/template-loader.php' );
 

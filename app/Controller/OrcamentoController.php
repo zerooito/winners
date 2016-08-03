983e3e773353ade28b886212cbbce7341b7d4594
Reginaldo Junior <reginaldo.junior@thirdlevel.com.br>
2016-08-03 18:50:14 -0300

    orçamento modulo

--- app/Controller/OrcamentoController.php
+++ app/Controller/OrcamentoController.php
@@ -32,10 +32,28 @@ class OrcamentoController extends AppController
 
 	public function pdf($vendaId)
 	{
-		// instantiate and use the dompdf class
+		$this->loadModel('Venda');
+		$this->loadModel('VendaItensProduto');
+
 		$dompdf = new Dompdf();
 
-		$html = $this->pegar_venda_como_html();
+		$dadosVenda = $this->Venda->find('all', 
+			array('conditions' =>
+				array(
+					'Venda.id' => $vendaId
+				)
+			)
+		);
+
+		$produtosVenda = $this->VendaItensProduto->find('all',
+			array('conditions' => 
+				array(
+					'VendaItensProduto.venda_id' => $vendaId
+				)
+			)
+		);
+
+		$html = $this->pegar_venda_como_html($dadosVenda, $produtosVenda);
 
 		$dompdf->loadHtml($html);
 
@@ -48,7 +66,7 @@ class OrcamentoController extends AppController
 		exit;
 	}
 
-	public function pegar_venda_como_html()
+	public function pegar_venda_como_html($dadosVenda, $produtosVenda)
 	{
 		$html = '';
 		$html .= '<html>';
@@ -60,7 +78,7 @@ class OrcamentoController extends AppController
 		$html .= '	<table style="background-color: #cacaca;"  width="100%" valign="center" align="center">';
 		$html .= '		<tr align="center">';
 		$html .= '			<td>';
-		$html .= '				<h2>Orçamento Pedido #1</h2>';
+		$html .= '				<h2>Orçamento Pedido #' . $dadosVenda[0]['Venda']['id'] . '</h2>';
 		$html .= '			</td>';
 		$html .= '		</tr> ';
 		$html .= '	</table>';
@@ -68,15 +86,11 @@ class OrcamentoController extends AppController
 		$html .= '	<table width="100%" valign="center" align="center">';
 		$html .= '		<tr  style="background-color: #ccc;">';
 		$html .= '			<td>Total: </td>';
-		$html .= '			<td>R$ 24,55</td>';
-		$html .= '		</tr>';
-		$html .= '		<tr  style="background-color: #fff;">';
-		$html .= '			<td>Desconto: </td>';
-		$html .= '			<td>R$ 4,55</td>';
+		$html .= '			<td>R$ ' . number_format($dadosVenda[0]['Venda']['valor'], 2, ',', '.') . '</td>';
 		$html .= '		</tr>';
 		$html .= '		<tr  style="background-color: #ccc;">';
 		$html .= '			<td>Valor a Pagar: </td>';
-		$html .= '			<td>R$ 4,55</td>';
+		$html .= '			<td>R$ ' . number_format($dadosVenda[0]['Venda']['valor'], 2, ',', '.') . '</td>';
 		$html .= '		</tr>';
 		$html .= '	</table>';
 		$html .= '	<br>';
@@ -98,20 +112,49 @@ class OrcamentoController extends AppController
 		$html .= '						<td>Valor</td>';
 		$html .= '					</tr>';
 		$html .= '';
-		$html .= '					<tr>';
-		$html .= '						<td>Alicate 14"</td>';
-		$html .= '						<td>1</td>';
-		$html .= '						<td>R$ 23,00</td>';
-		$html .= '					</tr>';
+		$desconto = 0;
+		foreach ($produtosVenda as $i => $produto) {
+			$produtoInfos = $this->getInfoProdutos($produto['VendaItensProduto']['produto_id']);
+			
+			$total = $produtoInfos['Produto']['preco'] * $produto['VendaItensProduto']['quantidade_produto'];
+			$total = number_format($total, 2, ',', '.');
+
+			$html .= '					<tr>';
+			$html .= '						<td>' . $produtoInfos['Produto']['nome'] . '</td>';
+			$html .= '						<td>' . $produto['VendaItensProduto']['quantidade_produto'] . '</td>';
+			$html .= '						<td>R$ ' . $total . '</td>';
+			$html .= '					</tr>';
+		}
 		$html .= '				</table>';
 		$html .= '			</td>';
 		$html .= '		</tr>';
 		$html .= '	</table>';
+		$html .= '	<br>';
+/*		$html .= '	<table width="100%" valign="center" align="center">';
+		$html .= '		<tr  style="background-color: #ccc;">';
+		$html .= '			<td>Total: </td>';
+		$html .= '			<td>R$ ' . number_format($desconto, 2, ',', '.') . '</td>';
+		$html .= '		</tr>';
+		$html .= '	</table>'; */
 		$html .= '';
 		$html .= '</body>';
 		$html .= '</html>';
 
 		return $html;
+	}
+
+	public function getInfoProdutos($produtoId)
+	{
+		$this->loadModel('Produto');
+
+		$response = $this->Produto->find('first', 
+			array('conditions' => array(
+					'Produto.id' => $produtoId
+				)
+			)
+		);
+
+		return $response;
 	}
 
 }
